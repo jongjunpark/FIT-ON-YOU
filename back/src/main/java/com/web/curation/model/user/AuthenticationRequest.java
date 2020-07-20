f99e24e6fb686038ed68cd0160ab81957c3bc40b
hyeongseoblim <iii3@cnu.ac.kr>
2020-07-20 14:13:39 +0900

    add to file?

---
+++ back/src/main/java/com/web/curation/model/user/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.web.curation.model.user;
+
+import lombok.*;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthenticationRequest {
+
+	String email;
+	String password;
+	String name;
+
+}
