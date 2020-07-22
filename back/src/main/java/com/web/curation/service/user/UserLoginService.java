15a9d4c939ddec0eff6253bb3d60213f5aa3630c
diatkdwls <diatkdwls@naver.com>
2020-07-22 18:40:00 +0900

    add JwtInterCeptor.java JwtService.java UserLoginService.java     edit WebCurationApplication.java AccountController.java UserDao.java     로그인시 토큰생성하여 넘겨줌     로그인시 이메일 유효성 검사     로그인시 비밀번호 유효성 검사     .S03P12B304-2     .S03P12B304-12

---
+++ back/src/main/java/com/web/curation/service/user/UserLoginService.java
@@ -0,0 +1,41 @@
+package com.web.curation.service.user;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.web.curation.dao.user.UserDao;
+import com.web.curation.model.UserDTO;
+import com.web.curation.model.user.User;
+
+import javassist.NotFoundException;
+
+/**
+ * 
+ * UserService
+ *
+ */
+
+@Service
+public class UserLoginService {
+	
+	@Autowired
+	private UserDao userDao;
+	
+	public UserDTO login(String email, String password) throws NotFoundException {
+		Optional<User> user = userDao.getUserByEmail(email);
+		if(user.isPresent()) {
+			
+			return new UserDTO(user.get());
+		}else {
+			return new UserDTO();
+		}
+//		if(email.equals("abc@def.net") && password.equals("1234")) {
+//			return new User(email,password);
+//
+//		}else {
+//			throw new RuntimeException("그런 사람 없어요");
+//		}
+	}
+}
