7d940e904612fcd79ed4cd02f0304dedb1c0e54b
parkjongjun <poiufgin7373@naver.com>
2020-07-24 14:10:18 +0900

    add navigation 기본틀만 형성 front .S03P12B304-32

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
+    <div class="nav-base"></div>
     <div id="nav">
+      <div class="nav-logo">Logo</div>
+      <div class="nav-user">
+        <div class="nav-user-icon nav-user-img">img</div>
+        <div class="nav-user-icon nav-go-mypage">mypage</div>
+        <div class="nav-user-icon nav-go-dm">DM</div>
+        <div class="nav-user-icon nav-go-alarm">Alarm</div>
+      </div>
     </div>
     <router-view/>
   </div>
