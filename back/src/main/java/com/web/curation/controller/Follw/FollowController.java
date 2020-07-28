c76a3cdefc4043258fa0e124d867feb53bff9c40
이준형 <ouosssssssa@naver.com>
2020-07-28 18:45:59 +0900

    nothing

---
+++ back/src/main/java/com/web/curation/controller/Follw/FollowController.java
@@ -0,0 +1,10 @@
+package com.web.curation.controller.Follw;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = { "*" })
+@RestController
+public class FollowController {
+
+}
