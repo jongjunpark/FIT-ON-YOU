49829057d0471e50da8d60102287ff11bda55b4d
diatkdwls <diatkdwls@naver.com>
2020-08-06 09:50:35 +0900

    edit AccountController.java UserDao.java Join.vue NewPassword.vue ProfileEdit.vue setting.vue mutations.js 파일업로드 일부 수정 마이페이지 정보 불러오고 소개 닉네임 변경 회원탈퇴 .S03P13B304-84

--- front/src/views/Join.vue
+++ front/src/views/Join.vue
@@ -472,11 +472,9 @@ export default {
       if (photoFile.files[0]) {
 
         frm.append("profile-img-edit", photoFile.files[0]);
-        axios.post('http://localhost:8080/api/account/addProfileImg',frm,{
-          headers:{
-              'Content-Type': 'multipart/form-data'
-          }
-        }).then( () =>{
+        frm.append("nicknem",this.nickname);
+        axios.post('http://localhost:8080/api/account/addProfileImg',frm,
+        ).then( () =>{
           console.log("1");
           this.$router.push('/feed')
           
--- front/src/views/ProfileEdit.vue
+++ front/src/views/ProfileEdit.vue
@@ -37,7 +37,7 @@
         </div>
       <p v-if="isChange && isChange2" class="nickname" @click="changeNickName">{{ user.nickname }} <img src="../assets/images/edit.png" alt="" class="edit-img"></p>
       <div v-if="isChange2 && isChange" class="edit-content" @click="changeContent">
-        <h3>자기소개 입니다. <img src="../assets/images/edit.png" alt="" class="edit-img"></h3>
+        <h3>{{user.selfintroduce}}<img src="../assets/images/edit.png" alt="" class="edit-img"></h3>
       </div>
       <div class="user-change-parent">
         <div v-if="isChange && isChange2" class="user-change" @click="goSettings">
@@ -66,7 +66,7 @@
 <script>
 import "../components/css/profileedit.css"
 import axios from 'axios'
-import { mapState } from 'vuex'
+import { mapState,mapMutations, mapActions } from 'vuex'
 
 export default {
   name: 'ProfileEdit',
@@ -78,17 +78,22 @@ export default {
       content:'',
       isChange2: true,
       followingCnt :'',
-      followedCnt :''
+      followedCnt :'',
+      tempNickName:'' ,
+      test:'',
     }
   },
   mounted(){
-      let ref=this;
-      let data = this.$cookies.get('auth-nickname');
-      let uri = data;
-      let uri_enc = encodeURIComponent(uri);
-      let uri_dec = decodeURIComponent(uri_enc);
-      let res = uri_dec;
-      axios.get('http://localhost:8080/api/mypage/',{
+
+    let ref=this;
+    let data = this.$cookies.get('auth-nickname');
+    let uri = data;
+    let uri_enc = encodeURIComponent(uri);
+    let uri_dec = decodeURIComponent(uri_enc);
+    let res = uri_dec;
+    this.tempNickName=res;
+    this.nickname=res;
+    axios.get('http://localhost:8080/api/mypage/',{
       params:{nickname:res}
     })
     .then((data)=>{
@@ -96,17 +101,36 @@ export default {
       ref.followingCnt=data.data.followingCnt;
     })
     .catch()
+
+  },
+  beforeUpdate(){
+    this.profileImg=this.user.profile_img;
+    console.log(this.profileImg,2)
   },
   computed: {
     ...mapState(['isLoggedIn', 'user'])
   },
   methods: {
+    ...mapMutations(['setUserIntro','setUserNick','setToken']),
+    ...mapActions(['sendUserInfo']),
     setProfileImg() {
+      let ref=this;
+
       var frm = new FormData();
       var photoFile = document.getElementById("profile-img-edit");
       frm.append("profile-img-edit", photoFile.files[0]);
-      console.log(photoFile.files[0]);
-      this.profileImg = URL.createObjectURL(photoFile.files[0]);
+      frm.append("nickname",this.nickname);
+      axios.post('http://localhost:8080/api/account/addProfileImg',frm)
+      .then((data)=>{
+        console.log(data)
+        ref.$cookies.set('auth-token', data.data.auth_token)
+        ref.setToken(data.data.auth_token)
+        ref.sendUserInfo();
+        ref.profileImg=ref.user.profileImg;
+      })
+      .catch()
+
+
     },
     changeNickName() {
       const wrapContainer = document.querySelector('.wrap-container')
@@ -131,6 +155,8 @@ export default {
       this.isChange = true;
     },
     change() {
+      let ref=this;
+
       const wrapContainer = document.querySelector('.wrap-container')
       const wrapNav = document.querySelector('#nav')
       const wrapBottom = document.querySelector('#nav2')
@@ -140,6 +166,24 @@ export default {
       wrapBottom.classList.remove('opacity-wrap')
       hidden.style.zIndex = -1000
       this.isChange = true;
+      const formData=new FormData();
+      formData.append("prev",this.tempNickName);
+      formData.append("cur",this.nickname);
+      axios.post('http://localhost:8080/api/account/nickchange',formData)
+      .then((data)=>{
+        console.log(data);
+        if(data.data.result.data=="success"){
+          ref.$cookies.set('auth-token', data.data.auth_token)
+          ref.setToken(data.data.auth_token)
+          ref.sendUserInfo();
+        }
+        else if(data.data.result.data=="fail"){
+          console.log("중복된 닉네임인 경우");
+        }
+      })
+      .catch()
+
+
     },
     changeContent() {
       const wrapContainer = document.querySelector('.wrap-container')
@@ -165,6 +209,8 @@ export default {
       this.isChange2 = true;
     },
     changeInput() {
+      let ref=this
+
       const wrapContainer = document.querySelector('.wrap-container')
       const wrapNav = document.querySelector('#nav')
       const wrapBottom = document.querySelector('#nav2')
@@ -174,6 +220,20 @@ export default {
       wrapBottom.classList.remove('opacity-wrap')
       hidden.style.zIndex = -1000
       this.isChange2 = true;
+
+      const formData = new FormData();
+      formData.append("nickname",this.nickname);
+      formData.append("selfintroduce",this.content);
+      axios.put('http://localhost:8080/api/account/selfintro',formData)
+      .then((data)=>{
+        console.log(data);
+        ref.$cookies.set('auth-token', data.data.auth_token)
+        ref.setToken(data.data.auth_token)
+        ref.sendUserInfo();
+        
+      })
+      .catch()
+
     },
     goSettings() {
       this.$router.push('/settings')
