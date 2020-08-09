3f689ffb4b953ec656ba2a0c1b3739eb2e4372ad
hyeongseoblim <iii3@cnu.ac.kr>
2020-08-09 20:18:02 +0900

    서버용 수정

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -206,7 +206,7 @@ export default {
     loginHandler() { 
       console.log(this.email);
       console.log(this.password);
-      axios.get('http://localhost:8080/api/account/login',{
+      axios.get('http://i3b304.p.ssafy.io:8080/api/account/login',{
         params:{email:this.email,
                   password:this.password},
       }).then( response => {
