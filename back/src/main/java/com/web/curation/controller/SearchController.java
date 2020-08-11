6ea30afb9e4e8eddf7bf069384260bd8f89accd7
hyeongseoblim <iii3@cnu.ac.kr>
2020-08-11 16:27:49 +0900

    "add responseData, edit Search files
    전체게시글 업로드 추가
    back
    .S03P13B304-98"

--- back/src/main/java/com/web/curation/controller/SearchController.java
+++ back/src/main/java/com/web/curation/controller/SearchController.java
@@ -1,5 +1,8 @@
 package com.web.curation.controller;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -12,14 +15,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.web.curation.dao.ArticletagDao;
+import com.web.curation.dao.BoardDao;
 import com.web.curation.dao.CurationDao;
+import com.web.curation.dao.ImageDao;
 import com.web.curation.dao.SearchDao;
 import com.web.curation.dao.UserDao;
+import com.web.curation.model.Articletag;
 import com.web.curation.model.BasicResponse;
+import com.web.curation.model.Board;
 import com.web.curation.model.Curation;
+import com.web.curation.model.ImageStore;
+import com.web.curation.model.ResponseData;
 import com.web.curation.model.Search;
 import com.web.curation.model.User;
 import com.web.curation.model.UserDTO;
@@ -28,6 +40,8 @@ import io.swagger.annotations.ApiOperation;
 
 @CrossOrigin(origins = { "*" })
 @RestController
+@RequestMapping("/search")
+
 public class SearchController {
 
 	@Autowired
@@ -36,8 +50,46 @@ public class SearchController {
 	CurationDao curationDao;
 	@Autowired
 	UserDao userDao;
+	@Autowired
+	BoardDao boardDao;
+	@Autowired
+	ArticletagDao articleTagDao;
+	@Autowired
+	ImageDao imageDao;
+	
+	@PostMapping("/")
+	@ApiOperation(value="페이지 업로드")
+	public Object uploadBoard (){
+		List<Board> temp = boardDao.getList();
+		List<ResponseData> result = new ArrayList<ResponseData>();
+		for (Board board : temp) {
+			ResponseData data = new ResponseData();
+			data.setArticles(new Board());
+			data.setImgs(new ArrayList<>());
+			data.setTags(new ArrayList<>());
+			
+			try {
+			List<ImageStore> imgs = imageDao.findImagestoreByArticleNoOrderByArticleNoDesc(board.getArticleNo());
+			data.setImgs(imgs);
+			}catch(Exception e){
+			e.printStackTrace();
+			}
+			try{
+				List<Articletag> tags = articleTagDao.findArticletagByArticleNoOrderByArticleNo(board.getArticleNo());
+				data.setTags(tags);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			data.setArticles(board);
+			
+			result.add(data);
+		}
+		
+		return new ResponseEntity<List<ResponseData>>(result, HttpStatus.OK);
+		
+	}
 
-	@GetMapping("/search/user")
+	@GetMapping("/user")
 	@ApiOperation(value = "유저 검색")
 	public Object searchUser(@Valid @RequestParam String username) {
 		final BasicResponse result = new BasicResponse();
@@ -56,7 +108,7 @@ public class SearchController {
 	}
 
 	// ariticleNo만 가져다쓰면됨 왜 모든 컬럼을 가져와야 작동하는 지 이해를 할수 없음 걍외우셈
-	@GetMapping("/search/hash")
+	@GetMapping("/hash")
 	@ApiOperation(value = "검색")
 	public Object searchHash(@Valid @RequestParam String username, @Valid @RequestParam String findContent) {
 
@@ -96,6 +148,6 @@ public class SearchController {
 
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
-	
 
 }
+
