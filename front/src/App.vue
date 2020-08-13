aeb652b782bd1885588c302bf4e039c27e3edc98
parkjongjun <poiufgin7373@naver.com>
2020-08-13 14:46:05 +0900

    feed bug fix 캐로셀에서 발생한 length err 해결 .S03P13B304-121

--- front/src/App.vue
+++ front/src/App.vue
@@ -31,7 +31,7 @@
     
     <router-view/>
     
-    <div v-if='isLoggedIn' id="nav2" >
+    <div v-show='isLoggedIn' id="nav2" >
       <div class="bottom-nav">
         <div class='menu-bar-list'>
           <div class="menu-bar-select"></div>
