41207181b90739ebeb44ca2521f351c79e536f83
hyeongseoblim <iii3@cnu.ac.kr>
2020-08-13 09:10:14 +0900

    conflict 해결

--- front/src/views/DM.vue
+++ front/src/views/DM.vue
@@ -3,39 +3,23 @@
     <div class="wrap-container-dm">
       <div class="dm-container-wrap">
         <div class="dm-container">
-          <div class="dm-container-message">
-            <img src="../assets/images/default-user.png" alt="" class="dm-container-message-img">
-            <h3>Username</h3>
-            <h5 class="in-text">마지막 메세지입니다.</h5>
-            <h5 class="in-text">1주전</h5>
-          </div>
-          <div class="dm-container-message">
-            <img src="../assets/images/default-user.png" alt="" class="dm-container-message-img">
-            <h3>Username2</h3>
-            <h5 class="in-text">안녕하세요.</h5>
-            <h5 class="in-text">1달전</h5>
-          </div>
-          <div class="dm-container-message">
-            <img src="../assets/images/default-user.png" alt="" class="dm-container-message-img">
-            <h3>Username</h3>
-            <h5 class="in-text">Lorem ipsum, dolor sit amet</h5>
-            <h5 class="in-text">1년전</h5>
-          </div>
         </div>
-
       </div>
       <div class="search-dm">
         <input type="text" name="" id="" class="search-dm-input" placeholder="검색">
+        <i class="fas fa-search search-img-dm"></i>
       </div>
-      <i class="fas fa-search search-img-dm"></i>
     </div>
   </div>
 </template>
 
 <script>
 import "../components/css/dm.css"
+import { mapState } from 'vuex'
 import firebase from 'firebase'
 import axios from 'axios'
+
+
 var db = firebase.firestore();
 
 export default {
@@ -43,6 +27,18 @@ export default {
   data() {
     return {
       lastMessage:{},
+      nickname: '',
+    }
+  },
+  mounted() {
+    this.defaultDark()
+  },
+  computed: {
+    ...mapState(['flag'])
+  },
+  watch: {
+    flag() {
+      this.defaultDark()
     }
   },
   updated() {
@@ -178,13 +174,20 @@ export default {
             });
           });
 
-        this.lastMessage=allMessages;
-        console.dir(new Date(this.lastMessage.createdAt.seconds*1000));
-      });
+        })
+        .catch(
+        )
     },
   },
   created(){
+    let nickdata = this.$cookies.get('auth-nickname')
+    let uri = nickdata;
+    let uri_enc = encodeURIComponent(uri);
+    let uri_dec = decodeURIComponent(uri_enc);
+    let res = uri_dec;
+    this.nickname = res
     this.getLastMessage();
   }
+
 }
 </script>
