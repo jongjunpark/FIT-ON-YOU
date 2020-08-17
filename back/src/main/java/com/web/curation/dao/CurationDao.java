ac42ffdb4db29831dc50300da6dd13858456edff
이준형 <ouosssssssa@naver.com>
2020-08-17 20:20:26 +0900

    4 hotfix curation And add delete curation function 큐래시션 5개씩 유지하는부분 문제 있어서 고침 그리고 검색기록삭제 만듦 back

--- back/src/main/java/com/web/curation/dao/CurationDao.java
+++ back/src/main/java/com/web/curation/dao/CurationDao.java
@@ -2,6 +2,8 @@ package com.web.curation.dao;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,8 +13,13 @@ import com.web.curation.model.Search;
 
 public interface CurationDao extends JpaRepository<Curation, String> {
 
-	@Query(value = "select * from curation where username = :username order by curationno DESC", nativeQuery = true)
+	@Query(value = "select * from curation where username = :username order by curationno", nativeQuery = true)
 	List<Curation> getCurationByUsername(String username);
-
+	
+	@Transactional
+	int deleteCurationByCurationno(@Param("int") int curationno);
+	
+	@Transactional
+	int deleteAllByUsername(@Param("String") String username);
 
 }
