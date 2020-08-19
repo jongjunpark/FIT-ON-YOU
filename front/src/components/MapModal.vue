4a1f5012fd34f4a43d6df30b97543fcafe98c608
parkjongjun <poiufgin7373@naver.com>
2020-08-20 08:54:48 +0900

    edit map div color

--- front/src/components/MapModal.vue
+++ front/src/components/MapModal.vue
@@ -58,7 +58,7 @@ export default {
 
               //인포윈도우로 장소에 대한 설명을 표시합니다
               var infowindow = new kakao.maps.InfoWindow({
-                  content: '<div style="width:150px;text-align:center;padding:6px 0;">' + HEAR + '</div>'
+                  content: '<div style="width:150px;text-align:center;padding:6px 0;color:black!important;">' + HEAR + '</div>'
               });
               infowindow.open(map, marker);
 
